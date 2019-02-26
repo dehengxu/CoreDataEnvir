3335ac879ffc27b5969af4c813fef46633a8c670
DehengXu <dehengxu@outlook.com>
2019-02-27 07:09:50 +0800

    Add: target for framework

---
+++ CoreDataEnvir iOS/CoreDataEnvir.h
@@ -0,0 +1,19 @@
+//
+//  CoreDataEnvir_iOS.h
+//  CoreDataEnvir iOS
+//
+//  Created by Deheng Xu on 2019/2/27.
+//  Copyright © 2019 Nicholas.Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreDataEnvir_iOS.
+FOUNDATION_EXPORT double CoreDataEnvir_iOSVersionNumber;
+
+//! Project version string for CoreDataEnvir_iOS.
+FOUNDATION_EXPORT const unsigned char CoreDataEnvir_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreDataEnvir_iOS/PublicHeader.h>
+
+
